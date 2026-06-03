c243f285c3bdeafe03ad78dbea1b38ee3c4cb2ad
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2026-06-03 10:10:28 +0200

    Set up GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://ntrp.github.io',
+	base: '/3d-printing-wiki',
 	integrations: [
 		starlight({
 			title: '3D Printing Wiki',
